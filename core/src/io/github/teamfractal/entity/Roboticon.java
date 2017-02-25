c4dc77e8edc3c7f6b2bc2f55e136ca3b3995cd40
Robin Stephenson <robin.mascros@gmail.com>
2017-02-25 13:56:24 +0000

    Created more tests for Roboticon, refactored roboticon

--- core/src/io/github/teamfractal/entity/Roboticon.java
+++ core/src/io/github/teamfractal/entity/Roboticon.java
@@ -1,23 +1,20 @@
-/**
- * @author DRTN
- * Team Website with download:
- * https://misterseph.github.io/DuckRelatedFractalProject/
- *
- * This Class contains either modifications or is entirely new in Assessment 3
- *
- * If you are in any doubt a complete changelog can be found here:
- * https://github.com/NotKieran/DRTN-Fractal/compare/Fractal_Initial...development
- *
- * And a more concise report can be found in our Change3 document.
- **/
+/*  JBT Assessment 4 Page: http://robins.tech/jbt/assfour.html
+ *  JBT Changes to this file:
+ *      Improved existing accessors with null pointer checks and more other invalid operations checks
+ *      Throw exceptions rather than returning false when things go wrong
+ *      Created some new accessors
+ */
 
 package io.github.teamfractal.entity;
 
 import io.github.teamfractal.entity.enums.ResourceType;
+import io.github.teamfractal.exception.AlreadyInstalledException;
+import io.github.teamfractal.exception.NotInstalledException;
 
 public class Roboticon {
     private int ID;
     private ResourceType customisation;
+
     private LandPlot installedLandPlot;
 
     Roboticon(int ID) {
@@ -57,18 +54,42 @@ public class Roboticon {
         return installedLandPlot != null;
     }
 
+    // JBT altered this method
     /**
-     *
+     * set the landplot this roboticon in installed on
+     * does NOT update the landplot to reflect this
      * @param landplot which roboticon is installed to
      * @return true if roboticon is installed, false if not
      */
-    public synchronized boolean setInstalledLandplot(LandPlot landplot) {
-        if (!isInstalled()) {
-            installedLandPlot = landplot;
-            return true;
+    public synchronized void setInstalledLandPlot(LandPlot landplot) throws AlreadyInstalledException {
+        if (isInstalled()) {
+            throw new AlreadyInstalledException("This roboticon is already installed");
         }
+        if (landplot == null) {
+            throw new NullPointerException("Given landplot cannot be null");
+        }
+        installedLandPlot = landplot;
+    }
 
-        System.out.println("Already installed to LandPlot! Cancel.");
-        return false;
+    // JBT created this method
+    /**
+     * get the LandPlot this roboticon is installed on
+     * @return the LandPlot
+     */
+    public LandPlot getInstalledLandPlot() {
+        return installedLandPlot;
+    }
+
+    // JBT created this method
+    /**
+     * remove this Roboticon from the LandPlot its currently installed on
+     * does NOT update the LandPlot to reflect this
+     * @throws NotInstalledException if the roboticon is not installed on a plot
+     */
+    public synchronized void removeFromLandPlot() throws NotInstalledException{
+        if (!isInstalled()) {
+            throw new NotInstalledException("This roboticon is not currently installed");
+        }
+        installedLandPlot = null;
     }
 }
--- test/src/io/github/teamfractal/entity/RoboticonTest.java
+++ test/src/io/github/teamfractal/entity/RoboticonTest.java
@@ -1,52 +1,117 @@
-/**
- * @author DRTN
- * Team Website with download:
- * https://misterseph.github.io/DuckRelatedFractalProject/
- *
- * This Class contains either modifications or is entirely new in Assessment 3
- *
- * If you are in any doubt a complete changelog can be found here:
- * https://github.com/NotKieran/DRTN-Fractal/compare/Fractal_Initial...development
- *
- * And a more concise report can be found in our Change3 document.
- **/
+/*  JBT Assessment 4 Page: http://robins.tech/jbt/assfour.html
+ *  JBT Changes to this file:
+ *      Added more tests to cover the sad paths / increase coverage on all paths
+ *      Added documentation for some existing tests
+ */
 
 package io.github.teamfractal.entity;
 
 import io.github.teamfractal.TesterFile;
 import io.github.teamfractal.entity.enums.ResourceType;
+import io.github.teamfractal.exception.AlreadyInstalledException;
+import io.github.teamfractal.exception.NotInstalledException;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class RoboticonTest extends TesterFile {
-	private Roboticon roboticon;
-	
-	@Before
-	public void setup() {
-		roboticon = new Roboticon(1);
-	}
-	
-	@Test
-	public void initialisationTest(){
-		assertEquals(roboticon.getCustomisation(), ResourceType.Unknown);
-		assertFalse(roboticon.isInstalled());
-	}
-	
-	@Test
-	public void customisationTest(){
-		roboticon.setCustomisation(ResourceType.ORE);
-		assertEquals(roboticon.getCustomisation(), ResourceType.ORE);
-	}
-	
-	@Test
-	public void installationTest(){
-		LandPlot plot = new LandPlot(0, 0, 0);
-		plot.installRoboticon(roboticon);
-		assertTrue(roboticon.isInstalled());
-	}
+    private Roboticon roboticon;
+    private final int roboticonID = 1;
 
-	
+    @Before
+    public void setup() {
+        roboticon = new Roboticon(roboticonID);
+    }
 
+    @Test
+    public void robotitonIDTest() {
+        assertEquals(roboticon.getID(), roboticonID);
+    }
+
+    @Test
+    public void initialisationTest(){
+        assertEquals(roboticon.getCustomisation(), ResourceType.Unknown);
+        assertFalse(roboticon.isInstalled());
+    }
+
+    @Test
+    public void customisationTest(){
+        roboticon.setCustomisation(ResourceType.ORE);
+        assertEquals(roboticon.getCustomisation(), ResourceType.ORE);
+    }
+
+    // JBT altered this test
+    /**
+     * landplot getters and setters happy path
+     */
+    @Test
+    public void setLandPlotTest() throws AlreadyInstalledException {
+        LandPlot plot = new LandPlot(0, 0, 0);
+        roboticon.setInstalledLandPlot(plot);
+        assertEquals(roboticon.getInstalledLandPlot(), plot);
+
+    }
+
+    // JBT created this test
+    /**
+     * setInstalledLandPlot should throw an exception if the given landplot is null
+     */
+    @Test(expected = NullPointerException.class)
+    public void setNullPlotTest() throws AlreadyInstalledException {
+        roboticon.setInstalledLandPlot(null);
+    }
+
+    // JBT Created this test
+    /**
+     * setInstalledLandPlot should throw an exception if roboticon is already installed somewhere
+     */
+    @Test(expected = AlreadyInstalledException.class)
+    public void alreadyInstalledSetPlotTest() throws AlreadyInstalledException {
+        LandPlot plot = new LandPlot(0, 0, 0);
+        LandPlot anotherPlot = new LandPlot(1,1,1);
+        roboticon.setInstalledLandPlot(plot);
+        roboticon.setInstalledLandPlot(anotherPlot);
+    }
+
+    /**
+     * getLandPlot should return the plot the roboticon is installed on, if installed
+     */
+    @Test
+    public void getLandPlotTest() throws AlreadyInstalledException {
+        LandPlot plot = new LandPlot(0, 0, 0);
+        roboticon.setInstalledLandPlot(plot);
+        assertEquals(roboticon.getInstalledLandPlot(), plot);
+    }
+
+    // JBT Created this test
+    /**
+     * getLandPlot should return null when the roboticon is not installed on a landplot
+     */
+    @Test
+    public void getLandPlotWhenNoneSetTest() {
+        assertNull(roboticon.getInstalledLandPlot());
+    }
+
+    // JBT Created this test
+    /**
+     * removeFromLandPlot should update isInstalled
+     */
+    @Test
+    public void removeFromPlotTest() throws AlreadyInstalledException, NotInstalledException {
+        LandPlot plot = new LandPlot(0, 0, 0);
+        roboticon.setInstalledLandPlot(plot);
+        roboticon.removeFromLandPlot();
+        assertFalse(roboticon.isInstalled());
+    }
+
+    // JBT Created this test
+    /**
+     * removeFromLandPlot should throw an error if called when the roboticon is not installed on a plot
+     */
+    @Test(expected = NotInstalledException.class)
+    public void removeFromPlotNotInstalledTest() throws NotInstalledException {
+        roboticon.removeFromLandPlot();
+    }
 }
