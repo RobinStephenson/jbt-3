33622a54bb4444f3dad8d7d5b535cf7d0a22d27e
Robin Stephenson <robin.mascros@gmail.com>
2017-02-25 17:43:40 +0000

    Added no args constructor for AlreadyInstalledElsewhereException

--- core/src/io/github/teamfractal/exception/AlreadyInstalledElsewhereException.java
+++ core/src/io/github/teamfractal/exception/AlreadyInstalledElsewhereException.java
@@ -1,6 +1,9 @@
 package io.github.teamfractal.exception;
 
 public class AlreadyInstalledElsewhereException extends Exception {
+    public AlreadyInstalledElsewhereException() {
+        super();
+    }
     public AlreadyInstalledElsewhereException(String message) {
         super(message);
     }
