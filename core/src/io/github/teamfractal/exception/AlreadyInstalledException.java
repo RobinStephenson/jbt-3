27dac0f24ca49bac995d851c5a4ff4578cf82880
Robin Stephenson <robin.mascros@gmail.com>
2017-02-25 13:14:10 +0000

    Created NotInstalledException and AlreadyInstalledException

---
+++ core/src/io/github/teamfractal/exception/AlreadyInstalledException.java
@@ -0,0 +1,7 @@
+package io.github.teamfractal.exception;
+
+public class AlreadyInstalledException extends Exception {
+    public AlreadyInstalledException(String message) {
+        super(message);
+    }
+}
