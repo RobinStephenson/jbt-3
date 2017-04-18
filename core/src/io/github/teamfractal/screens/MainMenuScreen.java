c6ba23318a55adbfbf6f25132cc595417fa3b8fd
Michael Walsh <Michaelwalsh223@hotmail.co.uk>
2017-04-18 17:46:16 +0100

    Updated menu background to new version

--- core/src/io/github/teamfractal/screens/MainMenuScreen.java
+++ core/src/io/github/teamfractal/screens/MainMenuScreen.java
@@ -6,6 +6,9 @@ package io.github.teamfractal.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
@@ -17,13 +20,16 @@ public class MainMenuScreen implements Screen {
 	final Stage stage;
 	final Table table;
 	private final HomeMainMenu homeMainMenu;
+	private Texture backgroundTex;
+	private SpriteBatch batch;
 
 	public MainMenuScreen(final RoboticonQuest game) {
 		this.game = game;
 		this.stage = new Stage(new ScreenViewport());
 		this.table = new Table();
 		table.setFillParent(true);
-
+        backgroundTex = new Texture("roboticon_images/menuBackground.png");
+        batch = new SpriteBatch();
 		homeMainMenu = new HomeMainMenu(game);
 		table.bottom().add(homeMainMenu);
 
@@ -39,6 +45,10 @@ public class MainMenuScreen implements Screen {
 	public void render(float delta) {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		batch.begin();
+		batch.draw(backgroundTex,0,0,1024,512);
+		batch.end();
 
 		stage.act(delta);
 		stage.draw();
@@ -67,5 +77,7 @@ public class MainMenuScreen implements Screen {
 	@Override
 	public void dispose() {
 		stage.dispose();
+		backgroundTex.dispose();
+		batch.dispose();
 	}
 }
